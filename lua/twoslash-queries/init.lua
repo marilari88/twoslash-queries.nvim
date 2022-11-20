508ce1bcedaed7a3b33b952299b7d2e9617ce399
Marco Ilari <marilari88@gmail.com>
2022-11-20 03:05:05 +0100

    Improve client request logic

--- lua/twoslash-queries/init.lua
+++ lua/twoslash-queries/init.lua
@@ -1,9 +1,8 @@
 M = {}
 
-local isEnabled = true
+local isEnabled = false
 
 local virtual_types_ns = vim.api.nvim_create_namespace("virtual_types")
-
 local get_buffer_number = function()
 	return vim.api.nvim_get_current_buf()
 end
@@ -14,32 +13,25 @@ local add_virtual_text = function(line, text)
 	})
 end
 
-local format_virtal_text = function(text)
+local format_virtual_text = function(text)
 	local converted = vim.lsp.util.convert_input_to_markdown_lines(text)
 	local escaped = string.gsub(converted[3], "\n   ", "")
 	return string.sub(escaped, 1, 120)
 end
 
-local get_hover_text = function(line, column)
+local get_hover_text = function(client, line, column)
 	local position = { line = line - 2, character = column }
 	local params = { textDocument = vim.lsp.util.make_text_document_params(get_buffer_number()), position = position }
-	vim.lsp.buf_request_all(get_buffer_number(), "textDocument/hover", params, function(data)
-		if data then
-			for _, client_response in pairs(data) do
-				if
-					client_response
-					and client_response.result
-					and client_response.result.contents
-					and client_response.result.contents.value
-				then
-					add_virtual_text(line, format_virtal_text(client_response.result.contents.value))
-				end
+	if client then
+		client.request("textDocument/hover", params, function(_, result)
+			if result and result.contents and result.contents.value then
+				add_virtual_text(line, format_virtual_text(result.contents.value))
 			end
-		end
-	end)
+		end)
+	end
 end
 
-local get_types = function()
+local get_types = function(client)
 	vim.api.nvim_buf_clear_namespace(get_buffer_number(), virtual_types_ns, 0, -1)
 	local lines = vim.api.nvim_buf_get_lines(get_buffer_number(), 0, -1, false)
 	local regex = vim.regex([[^\s*\/\/\s*\^\?]])
@@ -49,7 +41,7 @@ local get_types = function()
 
 		if match then
 			local column = string.find(lines[index], "%^")
-			get_hover_text(index, column)
+			get_hover_text(client, index, column)
 		end
 	end
 end
@@ -61,27 +53,25 @@ end
 
 M.enable = function()
 	isEnabled = true
-	get_types()
-end
-
-M.setup = function(config)
-	isEnabled = config.isEnabled or false
 end
 
 local activate_types_augroup = vim.api.nvim_create_augroup("activateTypes", { clear = true })
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "TabEnter", "InsertLeave", "TextChanged" }, {
-	pattern = "*",
-	group = activate_types_augroup,
-	callback = function()
-		if #vim.lsp.buf_get_clients() == 0 then
-			return
-		end
 
-		if isEnabled == false then
-			return
-		end
-		get_types()
-	end,
-})
+M.start = function(client)
+	isEnabled = true
+
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "TabEnter", "InsertLeave", "TextChanged" }, {
+		pattern = "*",
+		group = activate_types_augroup,
+		callback = function()
+			if isEnabled == false then
+				return
+			end
+			if client and client.server_capabilities.hoverProvider then
+				get_types(client)
+			end
+		end,
+	})
+end
 
 return M
--- plugin/twoslash-queries.lua
+++ plugin/twoslash-queries.lua
@@ -1,9 +1,8 @@
---[[ hi TypeVirtualText guifg=#CCCC00 ]]
 vim.api.nvim_set_hl(0, "TypeVirtualText", { fg = "#CCCC00" })
 
 vim.api.nvim_create_user_command("EnableTwoslashQueries", function()
 	require("twoslash-queries").enable()
-end, {})
+end, { nargs = 0, desc = "Enable two slash queries" })
 vim.api.nvim_create_user_command("DisableTwoslashQueries", function()
 	require("twoslash-queries").disable()
-end, {})
+end, { nargs = 0, desc = "Disable two slash queries" })
