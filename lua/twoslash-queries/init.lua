0709eb60b8a47207d20bb741121e893d4481f849
Marco Ilari <marilari88@gmail.com>
2022-11-21 10:50:56 +0100

    🐛 Fix render types in multiple buffers

--- lua/twoslash-queries/init.lua
+++ lua/twoslash-queries/init.lua
@@ -7,33 +7,37 @@ local get_buffer_number = function()
 	return vim.api.nvim_get_current_buf()
 end
 
-local add_virtual_text = function(line, text)
-	vim.api.nvim_buf_set_extmark(0, virtual_types_ns, line - 1, 0, {
+local add_virtual_text = function(buffer_nr, line, text)
+	vim.api.nvim_buf_set_extmark(buffer_nr, virtual_types_ns, line - 1, 0, {
 		virt_text = { { text, "TypeVirtualText" } },
 	})
 end
 
 local format_virtual_text = function(text)
 	local converted = vim.lsp.util.convert_input_to_markdown_lines(text)
-	local escaped = string.gsub(converted[3], "\n   ", "")
+	local joined_string = table.concat(converted, "", 3, #converted - 2)
+	local escaped = string.gsub(joined_string, "  ", " ")
 	return string.sub(escaped, 1, 120)
 end
 
-local get_hover_text = function(client, line, column)
+local get_hover_text = function(client, buffer_nr, line, column)
 	local position = { line = line - 2, character = column }
-	local params = { textDocument = vim.lsp.util.make_text_document_params(get_buffer_number()), position = position }
+	if not vim.api.nvim_buf_is_valid(buffer_nr) then
+		return
+	end
+	local params = { textDocument = vim.lsp.util.make_text_document_params(buffer_nr), position = position }
 	if client then
 		client.request("textDocument/hover", params, function(_, result)
 			if result and result.contents and result.contents.value then
-				add_virtual_text(line, format_virtual_text(result.contents.value))
+				add_virtual_text(buffer_nr, line, format_virtual_text(result.contents.value))
 			end
 		end)
 	end
 end
 
-local get_types = function(client)
-	vim.api.nvim_buf_clear_namespace(get_buffer_number(), virtual_types_ns, 0, -1)
-	local lines = vim.api.nvim_buf_get_lines(get_buffer_number(), 0, -1, false)
+local get_types = function(client, buffer_nr)
+	vim.api.nvim_buf_clear_namespace(buffer_nr, virtual_types_ns, 0, -1)
+	local lines = vim.api.nvim_buf_get_lines(buffer_nr, 0, -1, false)
 	local regex = vim.regex([[^\s*\/\/\s*\^\?]])
 
 	for index, line in pairs(lines) do
@@ -41,7 +45,7 @@ local get_types = function(client)
 
 		if match then
 			local column = string.find(lines[index], "%^")
-			get_hover_text(client, index, column)
+			get_hover_text(client, buffer_nr, index, column)
 		end
 	end
 end
@@ -57,8 +61,10 @@ end
 
 local activate_types_augroup = vim.api.nvim_create_augroup("activateTypes", { clear = true })
 
-M.start = function(client)
+M.attach = function(client, buffer_nr)
 	isEnabled = true
+	get_types(client, buffer_nr or 0)
+	print("mi attacco al buffer" .. buffer_nr)
 
 	vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "TabEnter", "InsertLeave", "TextChanged" }, {
 		pattern = "*",
@@ -68,7 +74,7 @@ M.start = function(client)
 				return
 			end
 			if client and client.server_capabilities.hoverProvider then
-				get_types(client)
+				get_types(client, buffer_nr or 0)
 			end
 		end,
 	})
