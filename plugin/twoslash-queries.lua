5365b6462f7d1e81f12b71c91bffc59e1cbdae45
Marco Ilari <marilari88@gmail.com>
2022-11-28 23:17:17 +0100

    ✨ Add user command for inspecting variable

--- plugin/twoslash-queries.lua
+++ plugin/twoslash-queries.lua
@@ -3,6 +3,25 @@ vim.api.nvim_set_hl(0, "TypeVirtualText", { fg = "#CCCC00" })
 vim.api.nvim_create_user_command("EnableTwoslashQueries", function()
 	require("twoslash-queries").enable()
 end, { nargs = 0, desc = "Enable two slash queries" })
+
 vim.api.nvim_create_user_command("DisableTwoslashQueries", function()
 	require("twoslash-queries").disable()
 end, { nargs = 0, desc = "Disable two slash queries" })
+
+vim.api.nvim_create_user_command("InspectTwoslashQueries", function()
+  -- get cursor position
+  local r,c = unpack(vim.api.nvim_win_get_cursor(0))
+
+  -- create a string line //
+  local two_slash_string = "//"
+  local i = 2
+  while i < c do
+    two_slash_string = two_slash_string..' '
+    i=i+1
+  end
+  two_slash_string = two_slash_string..'^?'
+
+  -- write string line
+  vim.api.nvim_buf_set_lines(0,r,r,false,{two_slash_string})
+
+end, { nargs = 0, desc = "Inspect variable under the cursor" })
