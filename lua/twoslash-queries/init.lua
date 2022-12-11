47b57fb5a4e3616751fc40f93149f7d0177e4b9d
Marco Ilari <marilari88@gmail.com>
2022-12-11 22:23:28 +0100

    feat: Add multiline text with flag

--- lua/twoslash-queries/init.lua
+++ lua/twoslash-queries/init.lua
@@ -2,6 +2,14 @@ M = {}
 
 M.Is_enabled = true
 
+M.multi_line = false
+
+M.setup = function(args)
+	if args.multi_line then
+		M.multi_line = args.multi_line
+	end
+end
+
 local query_regex = [[^\s*/\/\s*\^?]]
 
 local virtual_types_ns = vim.api.nvim_create_namespace("virtual_types")
@@ -9,14 +17,41 @@ local get_buffer_number = function()
 	return vim.api.nvim_get_current_buf()
 end
 
-local add_virtual_text = function(buffer_nr, line, text)
-	vim.api.nvim_buf_set_extmark(buffer_nr, virtual_types_ns, line - 1, 0, {
-		virt_text = { { text, "TypeVirtualText" } },
+local get_whitespaces_string = function(whitespaces)
+	local str = ""
+	local i = 0
+	while i < whitespaces do
+		str = str .. " "
+		i = i + 1
+	end
+	return str
+end
+
+local add_virtual_text = function(buffer_nr, position, lines)
+	local virt_text = {}
+	local virt_lines = {}
+
+	if M.multi_line == true then
+		virt_text = { { lines[1], "TypeVirtualText" } }
+		for i = 2, #lines do
+			virt_lines[i - 1] = { { get_whitespaces_string(position.character + 2) .. lines[i], "TypeVirtualText" } }
+		end
+	else
+		virt_text = { { lines, "TypeVirtualText" } }
+	end
+
+	vim.api.nvim_buf_set_extmark(buffer_nr, virtual_types_ns, position.line + 1, 0, {
+		virt_text = virt_text,
+		virt_lines = virt_lines,
 	})
 end
 
 local format_virtual_text = function(text)
-	local converted = vim.lsp.util.convert_input_to_markdown_lines(text)
+	local converted = vim.lsp.util.convert_input_to_markdown_lines(text, {})
+	if M.multi_line == true then
+		local selected_lines = vim.list_slice(converted, 3, #converted - 2)
+		return selected_lines
+	end
 	local joined_string = table.concat(converted, "", 3, #converted - 2)
 	local escaped = string.gsub(joined_string, "  ", " ")
 	return string.sub(escaped, 1, 120)
@@ -31,7 +66,7 @@ local get_hover_text = function(client, buffer_nr, line, column)
 	if client then
 		client.request("textDocument/hover", params, function(_, result)
 			if result and result.contents and result.contents.value then
-				add_virtual_text(buffer_nr, line, format_virtual_text(result.contents.value))
+				add_virtual_text(buffer_nr, position, format_virtual_text(result.contents.value))
 			end
 		end)
 	end
@@ -43,14 +78,14 @@ M.remove_queries = function()
 	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 	local regex = vim.regex(query_regex)
 
-  local removed_lines = 0
+	local removed_lines = 0
 
 	for index, line in pairs(lines) do
 		local match = regex:match_str(line)
 
 		if match then
-      vim.api.nvim_buf_set_lines(0,index-1 - removed_lines,index-removed_lines,true,{})
-      removed_lines=removed_lines + 1
+			vim.api.nvim_buf_set_lines(0, index - 1 - removed_lines, index - removed_lines, true, {})
+			removed_lines = removed_lines + 1
 		end
 	end
 end
