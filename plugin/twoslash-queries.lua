d376b2c379ac36b22618bea3298b1c5a8cdb85bd
undg <und3rdg@gmail.com>
2023-07-07 13:03:00 +0100

    Cleaning

--- plugin/twoslash-queries.lua
+++ plugin/twoslash-queries.lua
@@ -29,19 +29,22 @@ vim.api.nvim_create_user_command('TwoslashQueriesRemove', function()
 	require('twoslash-queries').remove_queries()
 end, { nargs = 0, desc = 'Remove all two slash queries in the current buffer' })
 
--- Fallback
+-- Fallback command for backward compatibility
 vim.api.nvim_create_user_command('EnableTwoslashQueries', function()
 	print('EnableTwoslashQueries commad is obsolete. Use TwoslashQueriesEnable')
 	vim.api.nvim_command('EnableTwoslashQueries')
 end, { nargs = 0, desc = '[Deprecated] Enable two slash queries' })
+
 vim.api.nvim_create_user_command('DisableTwoslashQueries', function()
 	print('DisableTwoslashQueries commad is obsolete. Use TwoslashQueriesDisable')
 	vim.api.nvim_command('DisableTwoslashQueries')
 end, { nargs = 0, desc = '[Deprecated] Disable two slash queries' })
+
 vim.api.nvim_create_user_command('InspectTwoslashQueries', function()
 	print('InspectTwoslashQueries commad is obsolete. Use TwoslashQueriesInspect')
 	vim.api.nvim_command('InspectTwoslashQueries')
 end, { nargs = 0, desc = '[Deprecated] Inspect two slash queries' })
+
 vim.api.nvim_create_user_command('RemoveTwoslashQueries', function()
 	print('RemoveTwoslashQueries commad is obsolete. Use TwoslashQueriesRemove')
 	require('twoslash-queries').remove_queries()
